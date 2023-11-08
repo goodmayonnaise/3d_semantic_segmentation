08d741ba953c37b2fe76afbbe4b84bc1dcfc4d45
JIYEON JEON <85322193+goodmayonnaise@users.noreply.github.com>
2023-11-08 09:36:23 +0900

    Create aspp.py

---
+++ models/aspp.py
@@ -0,0 +1,56 @@
+import torch
+from torch import nn
+
+class ASPP(nn.Module):
+    def __init__(self, in_dim, out_dim, d_size):
+        super(ASPP, self).__init__()
+
+        self.aspp1 = nn.Sequential(nn.Conv2d(in_dim, out_dim, 1, 1),
+                                   nn.BatchNorm2d(out_dim),
+                                   nn.ReLU())
+        self.aspp2 = nn.Sequential(nn.Conv2d(in_dim, out_dim, 3, 1, d_size, dilation=d_size),
+                                   nn.BatchNorm2d(out_dim),
+                                   nn.ReLU())
+        self.aspp3 = nn.Sequential(nn.Conv2d(in_dim, out_dim, 3, 1, d_size*2, dilation=d_size*2),
+                                   nn.BatchNorm2d(out_dim),
+                                   nn.ReLU())
+        self.aspp4 = nn.Sequential(nn.Conv2d(in_dim, out_dim, 3, 1, d_size*3, dilation=d_size*3),
+                                   nn.BatchNorm2d(out_dim),
+                                   nn.ReLU())
+        self.aspp5 = nn.Sequential(nn.AdaptiveAvgPool2d(1),
+                                   nn.Conv2d(in_dim, out_dim, 1, 1),
+                                   nn.BatchNorm2d(out_dim),
+                                   nn.ReLU())
+
+        self.out = nn.Sequential(nn.Conv2d(out_dim*5, out_dim, 1, 1),
+                                 nn.BatchNorm2d(out_dim),
+                                 nn.ReLU(),
+                                 nn.Conv2d(out_dim, out_dim, 1, 2))
+    
+        
+    def forward(self, x):
+        
+        x1 = self.aspp1(x)
+        x2 = self.aspp2(x)
+        x3 = self.aspp3(x)
+        x4 = self.aspp4(x)
+        x5 = self.aspp5(x)
+        _, _, w, h = x.shape
+        from torch.nn import functional as F
+        x5 = F.upsample(x5, (w, h), mode='bilinear')
+
+        cat = torch.cat([x1, x2, x3, x4, x5], 1)
+        out = self.out(cat)
+
+        return out 
+    
+
+
+if __name__ == "__main__":
+
+    aspp = ASPP(3, 32, 12)
+
+    input = torch.rand([5, 3, 256, 1248])
+    aspp(input)
+    print()
+        
